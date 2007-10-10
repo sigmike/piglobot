ee454d93cafad27f9187d5ab3cd7eafd35e534e7
Piglop <piglop@github>
2007-10-10 05:30:55 +0000

    commit en meme temps que publish
    git-svn-id: svn+ssh://ts/home/piglop/subversion/trunk@9 9b807a59-af8f-4ede-bc2b-4475ae9c2738

--- publish.rb
+++ publish.rb
@@ -8,5 +8,6 @@ ENV["MEDIAWIKI_RC"]="mediawikirc"
 wiki = MediaWiki.dotfile
 bot = Piglobot.new(wiki)
 
+system("svn", "ci", "-m", comment) || exit(1)
 bot.publish_spec comment
 bot.publish_code comment
